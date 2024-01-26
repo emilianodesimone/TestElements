b47d82c71611acb9b99552f9b2d319de19b311d2
Emiliano De Simone <emiliano.desim@gmail.com>
2024-01-26 12:14:42 +0200

    Implement multiple toggles choices

--- TestElements/ContentView.swift
+++ TestElements/ContentView.swift
@@ -1,24 +1,70 @@
-//
-//  ContentView.swift
-//  TestElements
-//
-//  Created by Emiliano De Simone on 26.1.2024.
-//
-
 import SwiftUI
 
-struct ContentView: View {
+struct OptionToggle: View {
+    var toggleRadius: Double
+    var id = UUID()
+    @Binding var isOn: Bool
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Circle()
+            .frame(width: toggleRadius, height: toggleRadius)
+            .foregroundColor(isOn ? .blue : .gray)
+            .onTapGesture {
+                isOn.toggle()
+            }
+        
+    }
+}
+
+struct Day {
+    let name: String
+    let abbreviation: String
+}
+
+class MultiOptionModel: ObservableObject {
+    @Published var options: [Bool]
+    
+    init(numberOfOptions: Int) {
+        options = Array(repeating: false, count: numberOfOptions)
+    }
+}
+
+struct OptionView: View {
+    static let numberOfOptions: Int = 7
+    @StateObject var model = MultiOptionModel(numberOfOptions: OptionView.numberOfOptions)
+    
+    
+    var body: some View {
+        HStack {
+            ForEach(0..<model.options.count, id: \.self) { index in
+                VStack {
+                    OptionToggle(toggleRadius: 20, isOn: $model.options[index])
+                    Text(weekDay(index, forFirstDay: 2))
+                        .foregroundColor(model.options[index] ? .green : .gray)
+                }
+            }
         }
         .padding()
+    }
+    
+    func weekDay(_ index: Int, forFirstDay firstDay: Int) -> String {
+        guard 1...7 ~= firstDay else { return "" }
+        guard 0...6 ~= index else { return "" }
+        
+        return Calendar.current.veryShortWeekdaySymbols[(firstDay + index - 1) % 7]
+    }
+    
+    static func week(forFirstDay firstDay: Int) -> [String] {
+        guard 1...7 ~= firstDay else { return [] }
+        
+        let indeces = (0..<7 ).map { (firstDay + $0 - 1) % 7 }
+        
+        let weekArray = indeces.compactMap { Calendar.current.veryShortWeekdaySymbols[$0] }
+        
+        return weekArray.count == 7 ? weekArray : []
     }
 }
 
 #Preview {
-    ContentView()
+    OptionView()
 }
