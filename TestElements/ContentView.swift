2d179ef3379d6259049079214d607f1aef449a41
Emiliano De Simone <emiliano.desim@gmail.com>
2024-01-26 10:28:16 +0200

    Initial Commit

---
+++ TestElements/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestElements
+//
+//  Created by Emiliano De Simone on 26.1.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestElements/TestElementsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestElementsApp.swift
+//  TestElements
+//
+//  Created by Emiliano De Simone on 26.1.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestElementsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
